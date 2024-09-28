f292ad460a6c55c2da6e7473901efd7a6a7a79ac
OmRenuse <147327022+OmRenuse@users.noreply.github.com>
2024-09-28 23:16:22 +0530

    repair of files

--- build.sh
+++ build.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-
-# Update pip
-echo "Updating pip..."
-python3.9 pip install -U pip
-
 # Install dependencies
+pip install -r requirements.txt
 
-echo "Installing project dependencies..."
-python3.9 -m pip install -r requirements.txt
+# Run migrations (for SQLite)
+python manage.py migrate --noinput
 
-# Make migrations
-echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-# Collect staticfiles
-echo "Collect static..."
-python3.9 manage.py collectstatic --noinput --clear
-
-echo "Build process completed!"
+# Collect static files
+python manage.py collectstatic --noinput
